2213a6b07dbb9290fbcd56e36ee024d79e8686ca
venkat munaga <venki.munaga@gmail.com>
2019-07-22 03:55:37 +0000

    my first commit

---
+++ Src/demo.portal/Areas/basic/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo.portal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("~/Areas/basic/Views/Home/Index.cshtml");
+        }
+
+        public ActionResult About()
+        {
+            return View("~/Areas/basic/Views/Home/About.cshtml");
+        }
+
+        public ActionResult Contact()
+        {
+            return View("~/Areas/basic/Views/Home/Contact.cshtml");
+        }
+    
+    }
+}
